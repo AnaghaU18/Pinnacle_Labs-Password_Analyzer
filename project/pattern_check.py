db4e701d824ab39113fffcb86b5adc8a77148da9
AnaghaU18 <166642111+AnaghaU18@users.noreply.github.com>
2024-07-12 11:02:44 +0530

    Update pattern_check.py

--- project/pattern_check.py
+++ project/pattern_check.py
@@ -2,12 +2,12 @@ import re
 import report
 
 def pswd_pattern(pswd):
-    #global score
+    """Check password for common patterns and update score and recommendations."""
     patterns = {
-            "Character repition": r'(.)\1{2,}',               # Character repetition
-            "Sequential numbers": r'012|123|234|345|456|567|678|789|890',  # Sequential numbers
-            "Sequential characters": r'abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz',  # Sequential characters
-            "Common keyboard patterns": r'qwerty|asdf|zxcv|qaz|wsx|edc|rfv|tgb|yhn|ujm|ik,|ol\.|pl;'  # Common keyboard patterns
+        "Character repetition": r'(.)\1{2,}',               # Character repetition
+        "Sequential numbers": r'012|123|234|345|456|567|678|789|890',  # Sequential numbers
+        "Sequential characters": r'abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz',  # Sequential characters
+        "Common keyboard patterns": r'qwerty|asdf|zxcv|qaz|wsx|edc|rfv|tgb|yhn|ujm|ik,|ol\.|pl;'  # Common keyboard patterns
     }
     
     found_vulnerable_pattern = False
@@ -18,5 +18,5 @@ def pswd_pattern(pswd):
             report.score -= 1
             found_vulnerable_pattern = True
     
-    if found_vulnerable_pattern:
+    if not found_vulnerable_pattern:
         report.pswd_strength("No common/vulnerable patterns")
